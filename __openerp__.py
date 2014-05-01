8df4744a60fbb2203b675f376c23549072d1e08a
Stephane Dereppe <stephane.dereppe@smals.be>
2014-05-01 15:53:18 +0200

    add first version of openerp manifest

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Test Module from STEDE",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Stephane Dereppe",
+    "category": "Test",
+    "description": """
+    This my first module in open ERP. It's just a test
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': True,
+    'autoinstall' : False
+#    'certificate': 'certificate',
+}
