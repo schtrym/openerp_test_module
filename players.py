26086c469e7687e85935bec06b33874c66b2cd17
Stephane Dereppe <stephane.dereppe@smals.be>
2014-05-12 17:35:22 +0200

    remove experience field and default values so that works

--- players.py
+++ players.py
@@ -4,12 +4,8 @@ from osv import fields
 class players(osv.osv):
     _name = 'test.players'
     _columns = {
-            'player_name': fields.char('Name', size = 30,required= True),
-            'experience' : fields.integer('Experience')
+            'player_name': fields.char('Name', size = 30,required= True)
             }
     _order = 'player_name' 
-    _defaults = {  
-        'player_name': 'default_name',
-        'experience' : '0'
-        }
+   
     _sql_constraints = [     ('PLAYER_UK', 'unique (player_name)', 'The name of the player must be unique !'),      ]
