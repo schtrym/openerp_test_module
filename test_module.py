3ca06ee9acec90d8e9ffb8a9adbaecaf4d7c373c
Stephane Dereppe <stephane.dereppe@smals.be>
2014-05-12 16:21:01 +0200

    adapt the model to represent a player object. with only a name and a year experience as attribute

--- test_module.py
+++ test_module.py
@@ -2,7 +2,14 @@ from osv import osv
 from osv import fields
 
 class test_class(osv.osv):
-    _name = 'test'
+    _name = 'test.players'
     _columns = {
-            'name': fields.char('Name', size = 30,required= True)
-                    }
+            'player_name': fields.char('Name', size = 30,required= True),
+            'experience' : fields.integer('Experience')
+            }
+    _order = 'player_name' 
+    _defaults = {  
+        'player_name': 'default_name',
+        'experience' : '0'
+        }
+    _sql_constraints = [     ('PLAYER_UK', 'unique (player_name)', 'The name of the player must be unique !'),      ]
